e2ca758356661da4bca1b8433f22880b31d45ee9
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-02-02 20:59:29 +0400

    First commit

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел.
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+Console.Clear();
+int[] array = new int[10];
+
+void FillArray(int[] array)
+
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length;i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(' ');
+    }
+}
+
+
+PrintArray(array);
