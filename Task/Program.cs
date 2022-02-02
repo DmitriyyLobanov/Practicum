7b90cb831be494ca9fb4e4626189d2142eab88d8
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-02-02 21:16:02 +0400

    Create special method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел.
 // Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
+using System;
 
 Console.Clear();
 int[] array = new int[10];
@@ -17,12 +17,28 @@ void FillArray(int[] array)
 }
 void PrintArray(int[] array)
 {
-    for (int i = 0; i < array.Length;i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i]);
         Console.Write(' ');
     }
 }
+void PrintEvenArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.Write(array[i]);
+            Console.Write(' ');
+        }
+    }
+}
 
-
+FillArray(array);
+Console.WriteLine("Изначально имеющийся массив целых чисел:");
 PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив чётных целых чисел:");
+PrintEvenArray(array);
+
